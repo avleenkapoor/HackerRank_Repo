dd28ec3be45f8d51f720ad83ae743baaea637663
Avleen Kapoor <avleen.k@techaspect.com>
2018-09-26 15:02:41 +0530

    Added Source Code for Challenge Merging Linked Lists

---
+++ Day_5/mergelinkedlist.js
@@ -0,0 +1,14 @@
+function mergeLists(head1, head2) {
+
+ if (head1 == null) return head2;
+  if (head2 == null) return head1;
+
+  if (head1.data < head2.data) {
+    head1.next = mergeLists(head1.next, head2);
+    return head1;
+  } else {
+   head2.next = mergeLists(head2.next, head1);
+    return head2;
+  }
+
+}
