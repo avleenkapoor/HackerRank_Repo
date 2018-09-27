7b400f69340adcfb186d3f2ebb9231bedb64f74b
Avleen Kapoor <avleen.k@techaspect.com>
2018-09-27 14:03:50 +0530

    Added Source Code for Inserting Node in Doubly Linked List

---
+++ Day_5/insertingnodeindoublylinkedlist.js
@@ -0,0 +1,26 @@
+
+function sortedInsert(head, data) {
+   
+    const node = new DoublyLinkedListNode(data);
+        
+        if (head == null) {
+            head = node;
+        } 
+    else if ((head.data >=node.data)) {
+        head.prev = node;
+        node.next = head;  
+        head = node; 
+    } 
+    else { 
+        var current = head;
+            while (current.next != null &&  
+               current.next.data < node.data) 
+                current = current.next; 
+   node.next = current.next; 
+   if (current.next != null) 
+            node.next.prev = node; 
+    current.next = node; 
+        node.prev = current; 
+    }
+return head;
+}
